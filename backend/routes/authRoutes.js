8e812871ac4fc03efc42456d5e31c0766ede02c0
IshaM1801 <ishamadlani824@gmail.com>
2025-09-02 15:55:26 +0530

    db changes as discussed

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
-
+//routes
 // Register (with email verification)
 router.post("/register", authController.register);
 

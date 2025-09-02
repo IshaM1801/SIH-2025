3d0c033700b57772c11d1e2a2e9edab2acf58ae2
evilstein-debug <tejaspathak2005@gmail.com>
2025-09-02 14:00:55 +0530

    new frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Login from './components/Login'
+import {Routes, Route} from 'react-router-dom'
+
+function App() {
+
+  return (
+    <>
+    <Routes>
+      <Route path='/login' element={<Login />} />
+    </Routes>
+    </>
+  )
+}
+
+export default App

7948fc45c6a05ac540192b34af2cd50603d8df8a
IshaM1801 <ishamadlani824@gmail.com>
2025-09-04 18:16:45 +0530

    merge conflicts resolved

--- backend/routes/issues.js
+++ backend/routes/issues.js
@@ -1,17 +1,6 @@
 // routes/issues.js
 const express = require("express");
 const router = express.Router();
-<<<<<<< Updated upstream
-const { getAllIssues, getUserIssues, createIssue } = require("../controllers/issuesController");
-const authenticateUser = require("../middleware/authMiddleware");
-
-// Now only relative paths
-router.get("/", getAllIssues);                  // GET /issues
-router.get("/user/:userId", getUserIssues);     // GET /issues/user/:userId
-router.post("/create", authenticateUser, createIssue); // POST /issues/create
-
-module.exports = router;
-=======
 const authMiddleware = require('../middleware/authMiddleware');
 
 const {
@@ -30,4 +19,3 @@ router.get('/dept', authMiddleware, getDeptIssues);  // new
 module.exports = router;
 
 //.
->>>>>>> Stashed changes

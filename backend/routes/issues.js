1d9fa23d5dc793b1b66a86265b475990ad232d9c
Siddhesh Todi <siddheshtodi@gmail.com>
2025-09-04 19:34:59 +0530

    Updated backend(Solved Duplicate issues problem)

--- backend/routes/issues.js
+++ backend/routes/issues.js
@@ -3,10 +3,11 @@ const express = require("express");
 const router = express.Router();
 const { getAllIssues, getUserIssues, createIssue } = require("../controllers/issuesController");
 const authenticateUser = require("../middleware/authMiddleware");
+const checkDuplicateIssue = require("../middleware/checkDuplicateIssue")
 
 // Now only relative paths
 router.get("/", getAllIssues);                  // GET /issues
 router.get("/user/:userId", getUserIssues);     // GET /issues/user/:userId
-router.post("/create", authenticateUser, createIssue); // POST /issues/create
+router.post("/create", authenticateUser, checkDuplicateIssue, createIssue); // POST /issues/create
 
 module.exports = router;
